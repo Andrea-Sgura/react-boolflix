f43d6feb6ba4ffb68dcaea747ca5b00b523a1ba9
Andrea-Sgura <andreasgura96@gmail.com>
2025-05-21 11:35:52 +0200

    [UPD] Creata componente Header.jsx, creata struttura, importata in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
-
+import axios from 'axios';
+import Header from './components/Header';
 
 function App() {
 
   return (
-    <><h1>Hello World</h1></>
+    <>
+      <Header />
+    </>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="container bg-dark text-center text-danger">
+            <div className="row">
+                <h1>Boolflix</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header
